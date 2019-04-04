51bb9958fb45d05df321881623b41d8514e49ee9
shushhy <49201453+shushhy@users.noreply.github.com>
2019-04-04 09:46:53 +0100

    Add files via upload

---
+++ dotfiles/backup.sh
@@ -0,0 +1,12 @@
+#! bin/sh
+
+cp ~/.xinitrc ~/Documents/dotfiles
+cp ~/.bash_profile ~/Documents/dotfiles
+cp ~/.config/compton.conf ~/Documents/dotfiles
+cp /etc/X11/xorg.conf ~/Documents/dotfiles
+cp ~/Pictures/wallpaper.* ~/Documents/dotfiles
+cp -R ~/.config/i3/ ~/Documents/dotfiles
+cp -R ~/.config/polybar/ ~/Documents/dotfiles
+cp -R ~/.config/rofi ~/Documents/dotfiles
+cp -R ~/.config/gtk-3.0 ~/Documents/dotfiles
+cp -R ~/.config/xfce4/terminal ~/Documents/dotfiles
